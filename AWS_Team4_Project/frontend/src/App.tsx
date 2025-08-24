bdf8269ca1b9a085d8c6a265394b52e4a2e172c3
yezee0609 <yezee0609@gmail.com>
2025-08-24 18:55:25 +0900

    Update App.tsx

--- AWS_Team4_Project/frontend/src/App.tsx
+++ AWS_Team4_Project/frontend/src/App.tsx
@@ -1,12 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import Landing from './pages/Landing.tsx'
 import MainApp from './pages/MainApp.tsx'
+import RandomChat from './pages/RandomChat.tsx'   
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/app" element={<MainApp />} />
-    </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/app" element={<MainApp />} />
+        <Route path="/random-chat" element={<RandomChat />} /> 
+      </Routes>
+    </Router>
   )
 }
